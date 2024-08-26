a8a44351052c7a5b609f1adce90a3ab6c314bba0
Felipe Inacio <felipeinacio.contato@gmail.com>
2024-08-26 17:30:10 -0300

    Manipulando Variáveis

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
 ﻿using dotnet.Models;
 
-Pessoa p = new()
-{
-    Nome = "Felipe",
-    Idade = 21
-};
+string apresentacao = "Ola, seja bem vindo";
 
-    p.Apresentar();
+int quantidade = 1;
+
+double altura = 1.80;
+
+decimal preco = 1.80M;
+
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+Console.WriteLine("Valor da variável altura: " + altura);
+Console.WriteLine("Valor da variável preco: " + preco);
+Console.WriteLine("Valor da variável condicao: " + condicao);
+
+// Pessoa p = new()
+// {
+//     Nome = "Felipe",
+//     Idade = 21,
+// };
+
+//     p.Apresentar();
--- obj/Debug/net8.0/dotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fbe01a394497315c83c585c58a86f77ae0398b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb7d1c241590124c2a272f2678eaea65b415b0bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
