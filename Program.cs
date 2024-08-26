b09efa200d3b0a638d02ddc040e07c6202b53512
Felipe Inacio <felipeinacio.contato@gmail.com>
2024-08-26 17:41:35 -0300

    Conhecendo o DataTime

--- Program.cs
+++ Program.cs
@@ -1,20 +1,24 @@
 ﻿using dotnet.Models;
 
-string apresentacao = "Ola, seja bem vindo";
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
 
-int quantidade = 1;
 
-double altura = 1.80;
+// string apresentacao = "Ola, seja bem vindo";
 
-decimal preco = 1.80M;
+// int quantidade = 1;
 
-bool condicao = true;
+// double altura = 1.80;
 
-Console.WriteLine(apresentacao);
-Console.WriteLine("Valor da variável quantidade: " + quantidade);
-Console.WriteLine("Valor da variável altura: " + altura);
-Console.WriteLine("Valor da variável preco: " + preco);
-Console.WriteLine("Valor da variável condicao: " + condicao);
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// Console.WriteLine("Valor da variável altura: " + altura);
+// Console.WriteLine("Valor da variável preco: " + preco);
+// Console.WriteLine("Valor da variável condicao: " + condicao);
 
 // Pessoa p = new()
 // {
