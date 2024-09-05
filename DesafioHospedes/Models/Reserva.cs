b722f7f0ed2b272900c90751599dc33182f8e797
Felipe Inacio <felipeinacio.contato@gmail.com>
2024-09-05 11:34:57 -0300

    Desafio Hóspedes Completo!

---
+++ DesafioHospedes/Models/Reserva.cs
@@ -0,0 +1,50 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite.Capacidade >= hospedes.Count )
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                Console.WriteLine("A capacidade é menor que a quantidade de Hospedes!");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = (decimal)DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                valor = valor * 0.90m;
+            }
+
+            return valor;
+        }
+    }
+}
