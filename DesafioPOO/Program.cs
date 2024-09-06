082052dd0743b7ee1ff177df40ea4aae49c9ee49
Felipe Inacio <felipeinacio.contato@gmail.com>
2024-09-06 18:38:42 -0300

    Desafio POO Completo!

---
+++ DesafioPOO/Program.cs
@@ -0,0 +1,11 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "12345678", modelo: "Modelo 1", imei: "10292222", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Safari");
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "09765443", modelo: "Modelo 2", imei: "12039487", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
