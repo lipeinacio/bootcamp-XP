88ac5c20a4563853ed4152350776738a03c9df2b
Felipe Inacio <felipeinacio.contato@gmail.com>
2024-10-11 16:43:23 -0300

    Adicionando Seed de Administração.

--- DesafioMinimal-API/Migrations/20241011194107_SeedAdministrador.Designer.cs
+++ DesafioMinimal-API/Migrations/20241011194107_SeedAdministrador.Designer.cs
@@ -2,6 +2,7 @@
 using Infraestrutura.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DesafioMinimalAPI.Migrations
 {
     [DbContext(typeof(DbContexto))]
-    partial class DbContextoModelSnapshot : ModelSnapshot
+    [Migration("20241011194107_SeedAdministrador")]
+    partial class SeedAdministrador
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
