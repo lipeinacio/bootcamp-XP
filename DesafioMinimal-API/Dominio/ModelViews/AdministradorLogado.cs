965b6027b80c555a3343eaf014bafff1ae139534
Felipe Inacio <felipeinacio.contato@gmail.com>
2024-10-16 17:02:05 -0300

    Configurando token JWT no projeto.

---
+++ DesafioMinimal-API/Dominio/ModelViews/AdministradorLogado.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DesafioMinimalAPI.Dominio.Enuns;
+
+namespace DesafioMinimalAPI.Dominio.ModelViews
+{
+    public record AdministradorLogado
+    {
+        public string Email { get; set; } = default!;
+        public string Perfil { get; set;} = default!;
+        public string Token { get; set;} = default!;
+
+    }
+}
